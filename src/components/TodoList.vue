d7d3741e5a94a5f6a2e103ec2116cc66835ede14
Alex Close <ajclose@gmail.com>
2017-12-06 14:52:51 -0600

    Render initial list on page

---
+++ src/components/TodoList.vue
@@ -0,0 +1,17 @@
+<template>
+<div>
+  <ul>
+    <li> Todo a </li>
+    <li> Todo b </li>
+    <li> Todo c </li>
+  </ul>
+</div>
+</template>
+
+<script type="text/javascript" >
+  export default {};
+  </script>
+
+  <style>
+  </style>
+  
